52cd8ab752223c4ec87bd1e86d2eb05a8949fe86
jwaltzjr <jwaltzjr@hotmail.com>
2017-07-19 18:03:58 -0500

    Add videolist / youtube api call

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDGlKuq93566a6-rz9gQyGPVaNdOuIn0_s';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {term: ''}
+    this.state = {videos: []};
+    YTSearch(
+      {key: API_KEY, term: 'test'},
+      (videos) => {
+        this.setState({videos: videos});
+      }
+    );
   }
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
