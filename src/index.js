1a6fe79d4471b987dd879e6dbd15a77873f0ff2b
jwaltzjr <jwaltzjr@hotmail.com>
2017-07-19 17:52:24 -0500

    Refactor App to class based view

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDGlKuq93566a6-rz9gQyGPVaNdOuIn0_s';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: ''}
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
