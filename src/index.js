0ba48dc6decd7c65af44d843c66d5f06df21066a
jwaltzjr <jwaltzjr@hotmail.com>
2017-07-19 19:00:39 -0500

    Add video search callback to SearchBAr

--- src/index.js
+++ src/index.js
@@ -33,10 +33,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={(selectedVideo) => {this.setState({selectedVideo})}}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos} />
       </div>
     );
